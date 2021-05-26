3ce463bc06209b6addba552a9d6d075f645c084c
Junyan Su <junyan.su@my.cityu.edu.hk>
2021-05-26 11:18:00 +0800

    add figure 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# pyplot-gallery
+# My pyplot-gallery
+
+This is a small gallery of pyplot examples. Hopefully I can retrieve the sample codes and save some time in the future.
+
+# Illustrations
+
+### Figure 1 [[SRC]](submodules/fig1/fig1.py)
+![figure 1](submodules/fig1/fig1.png)
---
+++ submodules/common.py
@@ -0,0 +1,21 @@
+"""
+This file store some functions for style adjustment
+to make the figures fit into publication templates.
+"""
+
+import matplotlib.pyplot as plt
+
+def use_acm_style():
+    rc_fonts = {
+    "font.family": "serif",
+    "font.size": 9,
+    #'figure.figsize': (5, 3),
+    "text.usetex": True,
+    #'text.latex.preview': True,
+    'text.latex.preamble': 
+        r"""
+        \usepackage[libertine]{newtxmath}
+        \usepackage{libertine}
+        """,
+    }
+    plt.rcParams.update(rc_fonts)
