d0e345bd0695e098a3ce5c27f364433a3e4ab365
sujunyan <sujy@shanghaitech.edu.cn>
2021-08-02 16:19:11 +0800

    update boxplot

--- fig3.py
+++ fig3.py
@@ -2,12 +2,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import style
+"""
+some notes:
+whisker: the vertical line below and above the box
+cap: the horizontal line above the whisker
+"""
 
 if __name__ == "__main__":
 	fig, ax = plt.subplots()
-	fig.set_size_inches(w=3.5, h=2.5)
+	#fig.set_size_inches(w=3.5, h=2.5)
 	np.random.seed(0)
-	data_mat = np.random.rand(1000,5)
+	n = 5
+	data_mat = np.random.rand(1000,n)
 	args = {
 		'notch':True,
 		'whis':1.5,
@@ -16,16 +23,44 @@ if __name__ == "__main__":
 		'showfliers':True,
 		'sym':'r+',
 		'showmeans':False,
-		'boxprops':{'color':'b'},
-		'whiskerprops':{'color':'black','ls':'--'},
-		'medianprops':{'c':'r'}
+		"notch": True, # the curve besides the median
+		'patch_artist':True, # enable facecolor
+		'boxprops':{
+			#'facecolor':style.google_colors[1],
+			#'facecolor':"None",
+			'edgecolor':style.google_colors[1],
+			'facecolor':style.google_colors[1]
+			},
+		'whiskerprops':{
+			'color':style.google_colors[1],
+			'ls':'-',
+			},
+		'capprops':{
+			'color':style.google_colors[1],
+		},
+		'medianprops':{
+			'c':style.google_colors[2],
+			'lw':3,
+			},
+		"widths":0.2,
+		
 	}
 	#ax.boxplot(data_mat, notch=True, whis=1.5, showbox=True, showcaps=True, showfliers=True, sym='r+', showmeans=False, boxprops={'color':'b'}, whiskerprops={'color':'black', 'ls':'--'}, medianprops={'c':'r'})
-	ax.boxplot(data_mat, **args)
+	range_ = np.arange(n)
+	offset = 0.14
+	ax.boxplot(data_mat, **args, positions=range_+offset)
+	for prop in ['boxprops', 'whiskerprops', 'capprops']:
+		args[prop]['color'] = style.google_colors[0]
+	args['boxprops']['facecolor'] = style.google_colors[0]
+
+	ax.boxplot(data_mat, **args, positions=range_-offset)
 	plt.xlabel("xlabel")
 	plt.ylabel("ylabel")
+	ax.set_xticks(range_)
+	ax.set_xticklabels(range_)
+
 	plt.grid(ls='--', axis='y')
 	plt.subplots_adjust(bottom=0.18, left=0.15, right=0.85)
-	fig.savefig("fig3.png", bbox_inches='tight')
+	fig.savefig("figs/fig3.png", bbox_inches='tight')
 
 # %%
--- style.py
+++ style.py
@@ -8,8 +8,9 @@ for a full list of matplotlib rc param
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+google_colors = ["#ea4335","#4285f4","#fbbc04","#34a853","#b8dae9","#f4cccc"]
 def use_google_style_colors():
-	mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=["#ea4335","#4285f4","#fbbc04","#34a853","#b8dae9","#f4cccc"]) 
+	mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=google_colors) 
 
 def use_bold_tex_style():
 	mpl.rc('font', family='serif', serif='cm10')
